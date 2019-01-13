8b1d01dcd174e4a21050c801915fc5053e8eb59f
dexterleow <gdleow@gmail.com>
2019-01-13 18:23:25 +0800

    add conteneditable to true to modify columns and cards

--- public/TrelloCard/TrelloCard.js
+++ public/TrelloCard/TrelloCard.js
@@ -6,7 +6,6 @@
             super();
 
             this.addEventListener('click', e => {
-                // this.editCard();
                 this.toggleDescription();
             });
 
@@ -31,28 +30,6 @@
             this.shadowRoot.querySelector('.trello-card__title').innerHTML = card.title;
             this.shadowRoot.querySelector('.trello-card__description').innerHTML = card.description;
             this.shadowRoot.querySelector('.trello-card__columnId').innerHTML = card.columnId;
-        }
-
-        editCard() {
-
-            // console.log(this.shadowRoot.querySelector('.trello-card__title'), "card title")
-            // var editCard = this.shadowRoot.querySelector('.trello-card__title');
-
-            // var bbbb = editCard.getAttribute('.trello-card__title');
-            // var att = document.createAttribute("input");
-            // console.log(bbbb, "bbbb");
-            // console.log(att, "att");
-            // editCard.setAttribute(bbbb,att);
-            // // var edit2 = editCard.setAttribute('class','info');
-            // console.log(editCard, "shadow edit card");
-            // // console.log(edit2, "shadow edit card2");
-
-
-            var editCard = this.shadowRoot.querySelector('.trello-card-container');
-
-            // this.shadowRoot.querySelector('.trello-card__title').innerHTML = "change";
-            // this.shadowRoot.querySelector('.trello-card__description').innerHTML = "change";
-
         }
 
         removeCard() {
